aefd3993beeabc6bc92af7985ca583980220dae0
Pierre Colle <piercus@gmail.com>
2019-03-29 11:56:35 +0100

    add simple example

---
+++ test/examples/simple-example.js
@@ -0,0 +1,67 @@
+// tfjs does not provide any read/write manipulation 
+// function on nodejs, so we nee to to this with pngjs
+
+const PNG = require('pngjs').PNG;
+const fs = require('fs');
+
+const fileToTensor = function(filename){
+	return new Promise((resolve, reject) => {
+		const inputPng = new PNG()
+		fs.createReadStream(filename)
+		  .pipe(inputPng)
+		  .on('parsed', function() {
+				const images = tf.tensor4d(inputPng.data, [1, inputPng.height, inputPng.width, 4])
+				resolve({images})
+			})
+			.on('error', reject)
+	})
+}
+
+const tensorToFile = function(filename, {images}){
+	return new Promise((resolve, reject) => {
+		const png = new PNG({
+			width: images.shape[2],
+			height: images.shape[1]
+		});
+		png.data = images.dataSync();
+		png
+			.pack()
+			.pipe(fs.createWriteStream(filename))
+			.on('error', reject)
+			.on('close', resolve)
+	})
+}
+
+// First you need a backend for image processing
+// this can be one of the following : 
+// * @tensorflow/tfjs 
+// * @tensorflow/tfjs-node
+// * @tensorflow/tfjs-node-gpu
+// * opencv4nodejs
+
+const tf = require('@tensorflow/tfjs-node');
+
+// Then initialize with the backend
+
+const ia = require('../..')(tf);
+
+// create an augmentation pipeline
+const basicAugmentation = ia.sequential([
+	// add a noise with a standard deviation of 15
+	ia.additiveNoise(15),
+	// rotate 30°
+	ia.affine({ rotate: 30 }),
+	// add a blur kernel of 3 pixel
+	ia.blur(3)
+]);
+
+fileToTensor('test/data/tfjs/lenna.png')
+.then(({images}) => {
+	return basicAugmentation.read({images});
+})
+.then(({images}) => {
+	return tensorToFile('test/data/tfjs/lenna.png', {images})
+})
+.then(() => {
+	console.log('done')
+})
