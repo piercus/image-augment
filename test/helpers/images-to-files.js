4d5a456c105ff03166dc525bd32600eb3867d20d
Pierre Colle <piercus@gmail.com>
2019-06-05 08:45:08 +0200

    add grayscale e2e test and fix blur 0 kernel

--- test/helpers/images-to-files.js
+++ test/helpers/images-to-files.js
@@ -8,7 +8,25 @@ const tensorToFile = function (filename, image) {
 			width: image.shape[2],
 			height: image.shape[1]
 		});
-		png.data = image.dataSync();
+		if (image.shape[3] === 4) {
+			png.data = image.dataSync();
+		} else {
+			const channels = image.shape[3];
+			const data = image.dataSync();
+			for (let i = 0; i < data.length; i++) {
+				const index4 = Math.floor(i / channels) * 4;
+				if (channels === 1) {
+					png.data[index4] = data[i];
+					png.data[index4 + 1] = data[i];
+					png.data[index4 + 2] = data[i];
+					png.data[index4 + 3] = 255;
+				} else if (channels === 3) {
+					png.data[index4] = data[i];
+					png.data[index4 + 3] = 255;
+				}
+			}
+		}
+
 		png
 			.pack()
 			.pipe(fs.createWriteStream(filename))
